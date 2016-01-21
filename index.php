d59d9e7a9f01a5aee773e8ae5a0333805e018ebc
Vilan <vitold55@ukr.net>
2016-01-21 13:17:40 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Sass</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+
+    <body>
+        <h1>Знайомство з SASS i Compass</h1>
+
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="">Наслідування</a>
+                </li>
+                <li>
+                    <a href="">Змінні</a>
+                </li>
+                <li>
+                    <a href="">Цикли</a>
+                </li>
+                <li>
+                    <a href="">Умови</a>
+                </li>
+            </ul>
+            <a href="">Text</a>
+        </div>
+
+        <button class="btn">Button</button>
+        <button class="warning">Button 1</button>
+        <button class="success">Button 2</button>
+    </body>
+</html>
